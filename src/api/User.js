91892163b43c5d72504362e343139a50f187b45d
Nguyen Dang Hoang Giang <gianghoang150503@gmail.com>
2025-03-08 15:44:36 +0700

    Updated API Auth

--- src/api/User.js
+++ src/api/User.js
@@ -9,65 +9,69 @@ const generateToken = (user) => {
 	return jwt.sign({id: user._id, email: user.email, role: user.role}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
 };
 
-// Register
+// Đăng ký tài khoản
 router.post('/register', async (req, res) => {
 	try {
-		let {name, email, dateOfBirth, gender, password, role} = req.body;
+		let {name, email, phone, dateOfBirth, gender, password, role} = req.body;
 		name = name.trim();
 		email = email.trim();
+		phone = phone.trim();
 		dateOfBirth = dateOfBirth.trim();
 		gender = gender.trim();
 		password = password.trim();
 
 		if (!name || !email || !dateOfBirth || !gender || !password) {
-			return res.status(400).json({status: 'Failed', message: 'Empty input field.'});
+			return res.status(400).json({status: 'Thất bại', message: 'Vui lòng không để trống bất kỳ trường nào.'});
 		}
-		if (!/^[a-zA-Z ]*$/.test(name)) {
-			return res.status(400).json({status: 'Failed', message: 'Invalid name entered.'});
-		}
+		// if (!/^[a-zA-Z ]*$/.test(name)) {
+		// 	return res.status(400).json({status: 'Thất bại', message: 'Tên không hợp lệ. Chỉ được chứa chữ cái và khoảng trắng.'});
+		// }
 		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-			return res.status(400).json({status: 'Failed', message: 'Invalid email entered.'});
+			return res.status(400).json({status: 'Thất bại', message: 'Email không hợp lệ. Vui lòng nhập đúng định dạng.'});
+		}
+		if (!/^\d{10,15}$/.test(phone)) {
+			return res.status(400).json({status: 'Thất bại', message: 'Số điện thoại không hợp lệ. Phải có từ 10 đến 15 chữ số.'});
 		}
 		if (!new Date(dateOfBirth).getTime()) {
-			return res.status(400).json({status: 'Failed', message: 'Invalid date of birth entered.'});
+			return res.status(400).json({status: 'Thất bại', message: 'Ngày sinh không hợp lệ. Vui lòng nhập đúng định dạng.'});
 		}
 		if (!['Male', 'Female', 'Other'].includes(gender)) {
-			return res.status(400).json({status: 'Failed', message: 'Invalid gender entered.'});
+			return res
+				.status(400)
+				.json({status: 'Thất bại', message: 'Giới tính không hợp lệ. Chỉ chấp nhận "Male", "Female" hoặc "Other".'});
 		}
 		if (password.length < 6) {
-			return res.status(400).json({status: 'Failed', message: 'Password is too short.'});
+			return res.status(400).json({status: 'Thất bại', message: 'Mật khẩu quá ngắn. Vui lòng nhập hơn 6 ký tự!'});
 		}
-
-		// Kiểm tra role hợp lệ
 		if (![0, 1].includes(role)) {
 			role = 1; // Nếu không hợp lệ, đặt mặc định là user
 		}
 
 		const existingUser = await User.findOne({email});
 		if (existingUser) {
-			return res.status(409).json({status: 'Failed', message: 'User with the provided email already exists'});
+			return res.status(409).json({status: 'Thất bại', message: 'Tài khoản đã tồn tại!'});
 		}
 
 		const hashedPassword = await bcrypt.hash(password, 10);
 
-		const newUser = new User({name, email, dateOfBirth, gender, password: hashedPassword, role});
+		const newUser = new User({name, email, phone, dateOfBirth, gender, password: hashedPassword, role});
 		const savedUser = await newUser.save();
 
 		// Tạo token cho user mới
 		const token = generateToken(savedUser);
 
 		res.status(201).json({
-			status: 'Success',
-			message: 'Register successful',
-			data: {id: savedUser._id, name, email, role},
+			status: 'Thành công',
+			message: 'Đăng ký tài khoản thành công!',
+			data: {id: savedUser._id, name, phone, dateOfBirth, gender, email, password: hashedPassword, role},
 			token,
 		});
 	} catch (error) {
-		res.status(500).json({status: 'Failed', message: 'An error occurred while processing registration'});
+		res.status(500).json({status: 'Thất bại', message: 'Đã xảy ra lỗi khi đăng ký tài khoản. Vui lòng thử lại sau!'});
 	}
 });
 
-// Login
+// Đăng nhập
 router.post('/login', async (req, res) => {
 	try {
 		let {email, password} = req.body;
@@ -75,30 +79,30 @@ router.post('/login', async (req, res) => {
 		password = password.trim();
 
 		if (!email || !password) {
-			return res.status(400).json({status: 'Failed', message: 'Empty credentials supplied'});
+			return res.status(400).json({status: 'Thất bại', message: 'Vui lòng không để trống email hoặc mật khẩu.'});
 		}
 
 		const user = await User.findOne({email});
 		if (!user) {
-			return res.status(401).json({status: 'Failed', message: 'Invalid credentials'});
+			return res.status(401).json({status: 'Thất bại', message: 'Email hoặc mật khẩu không chính xác!'});
 		}
 
 		const isPasswordMatch = await bcrypt.compare(password, user.password);
 		if (!isPasswordMatch) {
-			return res.status(401).json({status: 'Failed', message: 'Invalid password'});
+			return res.status(401).json({status: 'Thất bại', message: 'Mật khẩu không chính xác. Vui lòng thử lại!'});
 		}
 
 		// Tạo token
 		const token = generateToken(user);
 
 		res.status(200).json({
-			status: 'Success',
-			message: 'Login successful',
+			status: 'Thành công',
+			message: 'Đăng nhập thành công!',
 			data: {id: user._id, name: user.name, email: user.email, role: user.role},
 			token,
 		});
 	} catch (error) {
-		res.status(500).json({status: 'Failed', message: 'An error occurred while processing login'});
+		res.status(500).json({status: 'Thất bại', message: 'Đã xảy ra lỗi khi đăng nhập. Vui lòng thử lại sau!'});
 	}
 });
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,6 +15,13 @@ const UserSchema = new Schema(
 			lowercase: true,
 			trim: true,
 		},
+		phone: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			// match: [/^\d{10,15}$/, 'Invalid phone number'], // Số điện thoại từ 10-15 số
+		},
 		dateOfBirth: {
 			type: Date,
 			required: true,
